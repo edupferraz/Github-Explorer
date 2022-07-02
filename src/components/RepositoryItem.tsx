e6b4989e18f25c192cf5a9bab53a15b688017ba6
Eduardo Ferraz <edupferraz@gmail.com>
2022-07-02 07:28:47 -0300

    Using Typescript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,7 +1,13 @@
 
+interface RepositoryItemProps {
+    repository: {
+        name: string,
+        description: string
+        html_url: string
+    }    
+}
 
-
-export function RepositoryItem(props) {
+export function RepositoryItem(props: RepositoryItemProps) {
 
     return(
         <li>
